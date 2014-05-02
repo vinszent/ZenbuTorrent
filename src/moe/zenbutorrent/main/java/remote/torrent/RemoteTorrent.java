faaabf93b8d66625749376c03259471f69ed8e2c
Ippytraxx <ippytraxx@installgentoo.com>
2014-05-02 21:49:53 +0200

    Moved RemoteTorrentStatus into the RemoteTorrentClass.

--- src/moe/zenbutorrent/main/java/remote/torrent/RemoteTorrent.java
+++ src/moe/zenbutorrent/main/java/remote/torrent/RemoteTorrent.java
@@ -2,6 +2,15 @@ package moe.zenbutorrent.main.java.remote.torrent;
 
 public interface RemoteTorrent
 {
+    public final static String WAITING = "waiting";
+    public final static String CHECKING = "checking";
+    public final static String DOWNLOADING = "downloading";
+    public final static String SEEDING = "seeding";
+    public final static String PAUSED = "paused";
+    public final static String QUEUED = "queued";
+    public final static String ERROR = "error";
+    public final static String UNKOWN = "unkown";
+
     public String getStringId();
     public void setStringId(String infoHash);
 
@@ -11,20 +20,17 @@ public interface RemoteTorrent
     public String getTitle();
     public void setTitle(String title);
 
-    public String getFilepath();
-    public void setFilepath(String filepath);
+    public String getDownloadDirectory();
+    public void setDownloadDirectory(String directory);
 
-    public RemoteTorrentStatus getStatus();
-    public void setStatus(RemoteTorrentStatus status);
+    public String getStatus();
+    public void setStatus(String status);
 
     public long getSize();
     public void setSize(long size);
 
     public double getProgress();
     public void setProgress(double progress);
-
-    public long getRemaining();
-    public void setRemaining(long remaining);
 
     public long getDownloaded();
     public void setDownloaded(long downloaded);
